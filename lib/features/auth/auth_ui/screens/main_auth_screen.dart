b55f24da9468a671e791ec01e9a644e4188fd36c
Abdalrhman Abuwarda <eng.abdalrhman33@gmail.com>
2024-09-28 14:21:11 +0300

    fix app bar in main auth screen

--- lib/features/auth/auth_ui/screens/main_auth_screen.dart
+++ lib/features/auth/auth_ui/screens/main_auth_screen.dart
@@ -7,13 +7,15 @@ class MainAuthScreen extends StatefulWidget {
   State<MainAuthScreen> createState() => _MainAuthScreenState();
 }
 
-class _MainAuthScreenState extends State<MainAuthScreen> with SingleTickerProviderStateMixin {
+class _MainAuthScreenState extends State<MainAuthScreen>
+    with SingleTickerProviderStateMixin {
 
 
   @override
   void initState() {
     super.initState();
-    sl<AuthProvider>().tabController = TabController(length: 2, vsync: this); // Change length according to the number of tabs
+    sl<AuthProvider>().tabController = TabController(length: 2,
+        vsync: this); // Change length according to the number of tabs
   }
 
   @override
@@ -25,78 +27,80 @@ class _MainAuthScreenState extends State<MainAuthScreen> with SingleTickerProvid
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false, // Set this to true
-      // extendBody: true,
-
-      backgroundColor: ColorManager.soft,
-      appBar: AppBar(
-        elevation: 0,
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(AppSize.s20.height),
-          child: Align(
-            alignment: AlignmentDirectional.centerStart,
+        resizeToAvoidBottomInset: false, // Set this to true
+        backgroundColor: ColorManager.soft,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Align(
+            alignment: AlignmentDirectional.bottomStart,
             child: Container(
               width: AppSize.s210.width,
               padding: const EdgeInsets.all(0),
               margin: EdgeInsetsDirectional.only(
-                top: AppSize.s20.height,
+                bottom: AppSize.s20.height,
                 start: AppSize.s10.width,
               ),
-              decoration: const BoxDecoration(
-                color: ColorManager.transparent,
-                // border: null
+              decoration: BoxDecoration(
+                color: ColorManager.soft,
               ),
               child: Consumer<AuthProvider>(
-                builder: (context, auth, child) => TabBar(
-                  controller: auth.tabController,
-                  onTap: (index) => auth.changeCurrantTap(index),
-                  unselectedLabelColor: ColorManager.primary,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicator: const BoxDecoration(
-                    border: Border(
-                      bottom:
-                          BorderSide(color: ColorManager.secondary, width: 3),
-                    ),
-                    color: ColorManager.transparent,
-                  ),
-                  tabs: [
-                    Tab(
-                      child: Text(
-                        AppStrings.signIn,
-                        maxLines: 1,
-                        style: auth.tabController.index == 0
-                            ? titleSemiBoldSecondary
-                            : titleRegular,
+                builder: (context, auth, child) =>
+                    TabBar(
+                      controller: auth.tabController,
+                      onTap: (index) => auth.changeCurrantTap(index),
+                      unselectedLabelColor: ColorManager.primary,
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorColor: ColorManager.soft,
+                      dividerColor: ColorManager.soft,
+                      indicator: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                              color: ColorManager.secondary, width: 3),
+                        ),
+                        color: ColorManager.transparent,
                       ),
+                      tabs: [
+                        Tab(
+                          child: Text(
+                            AppStrings.signIn,
+                            maxLines: 1,
+                            style: auth.tabController.index == 0
+                                ? titleSemiBoldSecondary
+                                : titleRegular,
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            AppStrings.signUp,
+                            maxLines: 1,
+                            style: auth.tabController.index == 1
+                                ? titleSemiBoldSecondary
+                                : titleRegular,
+                          ),
+                        ),
+                      ],
                     ),
-                    Tab(
-                      child: Text(
-                        AppStrings.signUp,
-                        maxLines: 1,
-                        style: auth.tabController.index == 1
-                            ? titleSemiBoldSecondary
-                            : titleRegular,
-                      ),
-                    ),
-                  ],
-                ),
               ),
             ),
           ),
         ),
-      ),
-      body: LayoutBuilder(
-        builder: (_, __) => Consumer<AuthProvider>(
-          builder: (context, auth , child) => TabBarView(
-            controller: auth.tabController,
-            physics: const NeverScrollableScrollPhysics(),
-            children: [
-              LoginScreen(),
-              SignUpScreen(),
-            ],
-          ),
-        ),
-      ),
+        body: LayoutBuilder(
+        builder: (_, __) =>
+        Consumer<AuthProvider>(
+          builder: (context, auth, child) =>
+              TabBarView(
+                controller: auth.tabController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  LoginScreen(),
+                  SignUpScreen(),
+                ],
+              ),
+        )
+    ,
+    )
+    ,
     );
   }
 }
